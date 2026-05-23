0522f4eb34ff2245a252fcc9df8df932fcaa8f43
Yashi-makerr <ykesarwani41@gmail.com>
2026-05-23 23:45:56 +0530

    Fix production TypeScript build issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,40 +1,55 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI =
+  process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI");
+  throw new Error(
+    "Please define MONGODB_URI"
+  );
 }
 
 interface MongooseCache {
   conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
+
+  promise:
+    | Promise<typeof mongoose>
+    | null;
 }
 
 declare global {
   // eslint-disable-next-line no-var
-  var mongooseCache: MongooseCache | undefined;
+  var mongooseCache:
+    | MongooseCache
+    | undefined;
 }
 
-let cached = global.mongooseCache;
-
-if (!cached) {
-  cached = global.mongooseCache = {
+// ENSURE CACHE ALWAYS EXISTS
+const cached: MongooseCache =
+  global.mongooseCache || {
     conn: null,
     promise: null,
   };
+
+if (!global.mongooseCache) {
+  global.mongooseCache = cached;
 }
 
 export async function connectDB() {
+
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
+    cached.promise =
+      mongoose.connect(MONGODB_URI);
   }
 
-  cached.conn = await cached.promise;
+  cached.conn =
+    await cached.promise;
+
+
 
   return cached.conn;
 }
